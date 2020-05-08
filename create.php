07e7659a21d79d5345e2550bdd24dd1fa914f488
fweklar <65048271+fweklar@users.noreply.github.com>
2020-05-09 00:31:24 +0200

    Create create.php

---
+++ create.php
@@ -0,0 +1,16 @@
+if (!empty($_POST)) {	
+    $title = isset($_POST['title']) ? $_POST['title'] : '';
+    $desc = isset($_POST['desc']) ? $_POST['desc'] : '';
+	$username = md5($_SESSION['username']);
+    $stmt = $pdo->prepare('INSERT INTO polls VALUES (NULL, ?, ?, ?)');
+    $stmt->execute([$title, $desc, $username]);	
+    $poll_id = $pdo->lastInsertId();	
+    $answers = isset($_POST['answers']) ? explode(PHP_EOL, $_POST['answers']) : '';
+    foreach ($answers as $answer) {		
+        if (empty($answer)) continue;		
+        $stmt = $pdo->prepare('INSERT INTO poll_answers VALUES (NULL, ?, ?, 0)');
+        $stmt->execute([$poll_id, $answer]);
+    }	
+    $msg = 'Stworzono pomyślnie!';
+}
+?>
