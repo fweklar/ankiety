45574ba04412c352f3b881470dd8c43a5879ba49
fweklar <65048271+fweklar@users.noreply.github.com>
2020-05-09 00:35:05 +0200

    Update create.php

--- create.php
+++ create.php
@@ -1,5 +1,18 @@
 <?php
 
+
+  session_start(); 
+
+  if (!isset($_SESSION['username'])) {
+  	$_SESSION['msg'] = "Musisz się zalogować!";
+  	header('location: /phpoll/registration/login.php');
+  }
+  if (isset($_GET['logout'])) {
+  	session_destroy();
+  	unset($_SESSION['username']);
+  	header("location: /phpoll/registration/login.php");	
+  }
+
 include 'functions.php';
 $pdo = pdo_connect_mysql();
 $msg = '';
